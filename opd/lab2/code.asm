c68a2a492506d183ccbf3f85daa35c5401f7a354
Nikita Vasilev <nikita.vasilyev005@gmail.com>
2024-01-24 14:57:51 +0300

    opd (lab 2)

--- opd/lab2/code.asm
+++ opd/lab2/code.asm
@@ -1,45 +1,14 @@
-section .data
-    A dd 0x3178
-    B dd 0x3178
-    C dd 0x3182
-    D dd 0xA179
-    E dd 0xE183
-    output_buffer db 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-    
-section .text
-    global _start
-
-_start:
-    xor eax, eax
-    or eax, D
-    or eax, B
-    mov [A], eax
-    mov eax, [C]
-    sub eax, [A]
-    mov [E], eax
-    mov eax, [E]
-    mov ecx, 10
-    mov ebx, output_buffer + 9
-    mov byte [ebx], 0
-
-convert_loop:
-    dec ebx
-    xor edx, edx
-    div ecx
-    add dl, '0'
-    mov [ebx], dl
-    test eax, eax
-    jnz convert_loop
-
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, output_buffer
-    sub ecx, ebx
-    add ecx, 9
-    sub ecx, ebx
-    mov edx, 10
-    int 0x80
-
-    mov eax, 1
-    xor ebx, ebx
-    int 0x80
+ORG 0x177
+C: WORD 0x3178
+B: WORD 0x3178
+P: WORD 0x3182
+START: CLA
+OR 0x182
+OR 0x178
+ST 0x177
+LD 0x179
+SUB 0x177
+ST 0x183
+HLT
+A: WORD 0xA179
+R: WORD 0xE183
